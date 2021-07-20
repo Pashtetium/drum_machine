3c2cc3afb6483a4a52d6f69ed3cded2decf1cef6
Pashtetium <54268487+Pashtetium@users.noreply.github.com>
2021-07-20 14:50:46 +0500

    adding button press effects

--- src/DrumPad.jsx
+++ src/DrumPad.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+const activeStyle = {
+    backgroundColor: 'orange',
+    boxShadow: '0 3px orange',
+    height: 35,
+    marginTop: 12
+};
+  
+const inactiveStyle = {
+    backgroundColor: 'grey',
+    marginTop: 10,
+    boxShadow: '3px 3px 5px black'
+};
+
 class DrumPad extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            padStyle: inactiveStyle
+        }
         this.playSound = this.playSound.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.activatePad = this.activatePad.bind(this);
     }
     componentDidMount() {
         document.addEventListener("keydown", this.handleKeyPress);
@@ -22,15 +39,43 @@ class DrumPad extends React.Component {
         sound.currentTime = 0;
         sound.play();
         this.props.updateDisplay(this.props.clipId);
+        this.activatePad();
+        setTimeout(() => this.activatePad(), 100);
     }
-   
+    activatePad() {
+        if (this.props.power) {
+          if (this.state.padStyle.backgroundColor === 'orange') {
+            this.setState({
+              padStyle: inactiveStyle
+            });
+          } else {
+            this.setState({
+              padStyle: activeStyle
+            });
+          }
+        } else if (this.state.padStyle.marginTop === 12) {
+          this.setState({
+            padStyle: inactiveStyle
+          });
+        } else {
+          this.setState({
+            padStyle: {
+              height: 35,
+              marginTop: 12,
+              backgroundColor: 'grey',
+              boxShadow: '0 3px grey'
+            }
+          });
+        }
+      }
+
     render() {
         return (
             <div
                 className="drum-pad"
                 id={this.props.clipId}
                 onClick={this.playSound}
-                
+                style={this.state.padStyle}
             >
                 <audio 
                     className="clip"
--- src/PadBank.jsx
+++ src/PadBank.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DrumPad from './DrumPad';
 
+
 class PadBank extends React.Component {        
     constructor(props) {
         super(props);
